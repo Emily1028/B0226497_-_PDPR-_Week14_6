ba2d7875501772a49423cb29a8e0287615603206
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 23:36:03 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR__Week14_6
+{
+    class Program
+    {
+        public static void KG(int a)
+        {
+            if (a > 5)
+                Console.Write("網路購物重量:" + a + " 運費:"+(299+a*20));
+            else
+                Console.Write("網路購物重量:" + a + " 運費:" + (299 + a * 30));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B0226497_段昱如_PDPR__Week14_6");
+            int kg;
+            Console.Write("請輸入網路購物重量:");
+            kg = int.Parse(Console.ReadLine());
+            KG(kg);
+            Console.ReadLine();
+        }
+    }
+}
